193a8a6fdf2711cda9e91acdeb2d4e2f5c25bb58
k60081207-lang <k60081207@gmail.com>
2026-06-15 23:36:22 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -13,7 +13,7 @@ from memory import Memory
 from search import SearchEngine
 import telebot
 
-TOKEN =8871963243:AAGAyt1gq2UTTl-H70EGbabZuW3xHL900qc"
+TOKEN ="8871963243:AAGAyt1gq2UTTl-H70EGbabZuW3xHL900qc"
 bot = telebot.TeleBot(TOKEN)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
